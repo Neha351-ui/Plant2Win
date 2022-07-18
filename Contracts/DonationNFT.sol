be63ca29907b0c6c8856b37b0d208fe60046be98
Neha Singh <nehasingh2mau@gmail.com>
2022-07-18 12:52:58 +0530

    chainlink integration

--- Contracts/DonationNFT.sol
+++ Contracts/DonationNFT.sol
@@ -4,21 +4,92 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract LotteryNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract Plant2Win is
+    ERC721,
+    VRFConsumerBaseV2,
+    ERC721Enumerable,
+    ERC721URIStorage
+{
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    uint64 s_subscriptionId;
+
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+
+    bytes32 keyHash =
+        0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+
+    uint32 callbackGasLimit = 100000;
+
+    uint16 requestConfirmations = 3;
+
+    uint32 numWords = 2;
+
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
+    address s_owner;
+
+    uint256 public cost = 0.001 ether;
+
+    address public Winner1;
+    address public Winner2;
+
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("LotteryNFT", "eNFT") {}
+    constructor(uint64 subscriptionId)
+        ERC721("Plant2Win", "Plant")
+        VRFConsumerBaseV2(vrfCoordinator)
+    {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_owner = msg.sender;
+        s_subscriptionId = subscriptionId;
+    }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    // Assumes the subscription is funded sufficiently.
+    function AnnounceWinners() external onlyOwner {
+        // Will revert if subscription is not set and funded.
+        s_requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomWords = randomWords;
+        Winner1 = ownerOf((s_randomWords[0] % totalSupply()) + 1);
+        Winner2 = ownerOf((s_randomWords[1] % totalSupply()) + 1);
+    }
+
+    function safeMint(address to, string memory uri) public payable {
+        require(msg.value >= cost);
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+    }
+
+    //only owner
+    function setCost(uint256 _newCost) public onlyOwner {
+        cost = _newCost;
+    }
+
+    function withdraw() public payable onlyOwner {
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(success);
     }
 
     // The following functions are overrides required by Solidity.
@@ -54,5 +125,10 @@ contract LotteryNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == s_owner);
+        _;
     }
 }
