12c22dcb47cc3da5220eaf0704774cfec972b3da
Nikhil Verma 360 <nehasingh2mau@gmail.com>
2022-07-18 11:01:55 +0530

    rearrange files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-
-import './App.css';
-//import Contact from './Component/Contact';
 import Footer from './Component/Footer';
 import Main from './Component/Main';
 import Nav from './Component/Nav';
@@ -8,11 +5,9 @@ import Nav from './Component/Nav';
 const App = () => {
   return (
     <>
-      <Nav/>
-
-      <Main/>
-       {/* <Contact/>  */}
-       <Footer/>
+      <Nav />
+      <Main />
+      <Footer />
     </>
 
   );
